c766ea120efc27981c78b9305c10fe1129780deb
Hsin Home <stxin01@hotmail.com>
2021-07-09 15:35:50 +0800

    Window小工具

---
+++ HsinTools/HsinTools.WindowTool/Common/NetFwManager.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using NetFwTypeLib;
+
+namespace HsinTools.WindowTool.Common
+{
+    public static class NetFwManger
+    {
+
+        public class FwParam
+        {
+            public string Name { get; set; }
+
+            public string LocalAddress { get; set; }
+
+            public string LocalPorts { get; set; }
+
+            public string RemoteAddress { get; set; }
+
+            public string RemotePorts { get; set; }
+
+            public string Protocol { get; set; }
+
+            public bool IsEnable { get; set; }
+
+            public NET_FW_RULE_DIRECTION_ Direction { get; set; }
+
+            public NET_FW_ACTION_ Action { get; set; }
+
+            public string Description { get; set; }
+        }
+
+        /// <summary>
+        /// 添加防火墙例外端口
+        /// </summary>
+        /// <param name="param">参数名</param>
+        public static void NetFwAddPorts(FwParam param)
+        {
+            //创建防火墙策略类的实例
+            INetFwPolicy2 policy2 = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            //创建防火墙规则类的实例
+            INetFwRule rule = (INetFwRule)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwRule"));
+            //为规则添加名称
+            rule.Name = param.Name;
+            //为规则添加描述
+            rule.Description = param.Description;
+            //选择入站规则还是出站规则，IN为入，OUT为出
+            rule.Direction = param.Direction;
+            //为规则添加协议类型
+            switch (param.Protocol.ToUpper())
+            {
+                case "TCP":
+                    rule.Protocol = (int)NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_TCP;
+                    break;
+                case "UDP":
+                    rule.Protocol = (int)NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_UDP;
+                    break;
+                default:
+                    rule.Protocol = (int)NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_ANY;
+                    break;
+            }
+
+            //为规则添加应用程序（注意这里是应用程序的绝对路径名）
+            //rule.ApplicationName = exePath;
+            //为规则添加本地IP地址
+            rule.LocalAddresses = param.LocalAddress;
+            //为规则添加本地端口
+            rule.LocalPorts = param.LocalPorts;
+            //为规则添加远程IP地址
+            rule.RemoteAddresses = param.RemoteAddress;
+            //为规则添加远程端口
+            rule.RemotePorts = param.RemotePorts;
+            //设置规则是阻止还是允许（ALLOW=允许，BLOCK=阻止）
+            rule.Action = param.Action;
+            //是否启用规则
+            rule.Enabled = param.IsEnable;
+            try
+            {
+                //添加规则到防火墙策略
+                policy2.Rules.Add(rule);
+            }
+            catch (Exception e)
+            {
+            }
+
+        }
+
+        /// <summary>
+        /// 删除防火墙例外端口
+        /// </summary>
+        /// <param name="ruleName">规则名称</param>
+        public static void NetFwDelApps(string ruleName)
+        {
+            //创建防火墙策略类的实例
+            var policy2 = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
+            try
+            {
+                //根据规则名称移除规则
+                policy2.Rules.Remove(ruleName);
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+    }
+
+}
---
+++ HsinTools/HsinTools.WindowTool/Views/Main.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using HsinTools.WindowTool.Common;
+using NetFwTypeLib;
+using Notification.Wpf;
+
+namespace HsinTools.WindowTool.Views
+{
+    /// <summary>
+    /// Main.xaml 的交互逻辑
+    /// </summary>
+    public partial class Main : Window
+    {
+
+        public Main()
+        {
+            InitializeComponent();
+            Loaded += WindowLoaded;
+            Closed += WindowClosed;
+        }
+
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+            // 启动时隐藏窗口
+            //Visibility = Visibility.Hidden;
+
+            var notificationManager = new NotificationManager();
+
+            if (!Hotkey.Regist(this, Hotkey.HotkeyModifiers.MOD_CONTROL, Key.R, SwitchGw))
+                notificationManager.Show("提示", "注册快捷键失败(CTRL+R)", NotificationType.Error, expirationTime: TimeSpan.FromSeconds(3));
+
+            if (!Hotkey.Regist(this, Hotkey.HotkeyModifiers.MOD_CONTROL, Key.Q, SwitchVisible))
+                notificationManager.Show("提示", "注册快捷键失败(CTRL+Q)", NotificationType.Error, expirationTime: TimeSpan.FromSeconds(3));
+
+            notificationManager.Show("提示", "程序已启动", NotificationType.Notification, expirationTime: TimeSpan.FromSeconds(3));
+        }
+
+        private void WindowClosed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void SwitchGw()
+        {
+            // 切换防火墙的允许与拒绝连接
+            FwIsAllow.IsChecked = !FwIsAllow.IsChecked;
+            SetGsFw(false);
+
+            var notificationManager = new NotificationManager();
+            notificationManager.Show("提示", $"已{(FwIsAllow.IsChecked.Value ? "允许" : "拒绝")}防火墙规则连接", NotificationType.Notification, expirationTime: TimeSpan.FromSeconds(2));
+        }
+
+        private void SwitchVisible()
+        {
+            Visibility = Visibility == Visibility.Hidden ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        private void SetGsFw(bool isNotice = true)
+        {
+            var fwProtocolSelectedItem = FwProtocol.SelectedItem as TextBlock;
+            var protocol = fwProtocolSelectedItem.Text;
+
+            var fwRuleSelectedItem = FwRule.SelectedItem as TextBlock;
+            var direction = fwRuleSelectedItem.Text;
+
+            var localAddress = FwLocalIp.Text;
+            var localPorts = FwLocalPort.Text;
+            var remoteAddress = FwRemoteIp.Text;
+            var remotePorts = FwRemotePort.Text;
+            var ruleName = FwName.Text;
+            var isEnable = FwIsEnable.IsChecked ?? true;
+            var isAllow = FwIsAllow.IsChecked ?? true;
+
+            NetFwManger.NetFwDelApps(ruleName);
+            NetFwManger.NetFwAddPorts(new NetFwManger.FwParam
+            {
+                Name = ruleName,
+                LocalAddress = localAddress,
+                LocalPorts = localPorts,
+                RemoteAddress = remoteAddress,
+                RemotePorts = remotePorts,
+                Protocol = protocol,
+                IsEnable = isEnable,
+                Direction = direction == "出站规则" ? NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_OUT : NET_FW_RULE_DIRECTION_.NET_FW_RULE_DIR_IN,
+                Action = isAllow ? NET_FW_ACTION_.NET_FW_ACTION_ALLOW : NET_FW_ACTION_.NET_FW_ACTION_BLOCK,
+                Description = ""
+            });
+
+            LbStatus.Content = $"[{DateTime.Now:yyyy/MM/dd HH:mm:ss}] 修改成功";
+            if (isNotice)
+            {
+                var notificationManager = new NotificationManager();
+                notificationManager.Show("提示", "规则修改成功", NotificationType.Notification, expirationTime: TimeSpan.FromSeconds(2));
+            }
+        }
+
+        private void saveGsFw_Click(object sender, RoutedEventArgs e)
+        {
+            SetGsFw();
+        }
+    }
+}
