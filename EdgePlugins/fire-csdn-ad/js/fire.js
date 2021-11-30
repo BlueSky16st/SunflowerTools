774501e0dcb8c2b36e341eb6a312fbdda3849efd
Hsin Air <stxin01@hotmail.com>
2021-11-30 15:14:36 +0800

    添加间隔查询

--- EdgePlugins/fire-csdn-ad/js/fire.js
+++ EdgePlugins/fire-csdn-ad/js/fire.js
@@ -1,7 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
-  var tbAd = document.getElementsByClassName('toolbar-advert');
-  if (tbAd !== undefined && tbAd.length > 0) {
-    tbAd[0].style.display = 'none';
-    console.log('已经帮你关掉恶心的广告了!');
-  }
+  var intervalCountdown = 0;
+  var intervalId = setInterval(() => {
+    var tbAd = document.getElementsByClassName('toolbar-advert');
+    if (tbAd !== undefined && tbAd.length > 0) {
+      tbAd[0].style.display = 'none';
+      console.log('已经帮你关掉恶心的广告了!');
+      clearInterval(intervalId);
+      return;
+    }
+    // console.log('每隔一段时间查找一次...');
+    intervalCountdown++;
+
+    if (intervalCountdown >= 20) clearInterval(intervalId);
+  }, 500);
 });
